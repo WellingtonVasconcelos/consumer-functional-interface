854ead450d239a896bc3cd783b06eac1f0dc411c
Wellington Vasconcelos <wltvasc@hotmail.com>
2022-03-17 18:04:21 -0300

    Consumer functional interface

---
+++ src/aplication/Program.java
@@ -0,0 +1,35 @@
+package aplication;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		double factor = 1.1;
+		
+		//Expressão lambda declarada usando Consumer
+		//Consumer<Product> cons = p -> p.setPrice(p.getPrice() * factor);
+		
+		
+		//Expressão lambda inline usando Consumer		
+		list.forEach(p -> p.setPrice(p.getPrice() * factor));
+		
+		list.forEach(System.out::println);
+
+	}
+
+}
---
+++ src/entities/Product.java
@@ -0,0 +1,46 @@
+package entities;
+
+public class Product {
+
+	private String name;
+	private Double price;
+	
+	public Product() {
+	}
+
+	public Product(String name, Double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	//Método estático trabalha com o produto que você passa com argumento
+	public static void staticPricetUpdate(Product p) {
+		p.setPrice(p.getPrice() * 1.1);
+	}
+	
+	//Método não estático é o método da estácia. Ele trabalha com o proprio objeto
+	public void nonStaticPriceUpdate() {
+		price = price * 1.1;
+	}
+	
+	@Override
+	public String toString() {
+		return  name + ", " +  String.format("%.2f",  price);
+	}
+}
